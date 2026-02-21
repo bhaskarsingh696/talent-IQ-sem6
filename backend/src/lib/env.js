464608a57e6e82ff4d5b83e730d57a12312c2e71
bhaskar singh <146510258+bhaskarsingh696@users.noreply.github.com>
2026-02-21 12:14:50 +0530

    add CLIENT_URL to env for production

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -2,7 +2,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const ENV= {
-  PORT:process.env.PORT,
-  DB_URL:process.env.DB_URL,
+import dotenv from "dotenv";
+dotenv.config();
+
+export const ENV = {
+  PORT: process.env.PORT,
+  DB_URL: process.env.DB_URL,
+  CLIENT_URL: process.env.CLIENT_URL,
+  NODE_ENV: process.env.NODE_ENV,
 };
