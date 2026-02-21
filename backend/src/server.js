4e4713a6944ab523339dd93982c3e4251569a39e
bhaskar singh <146510258+bhaskarsingh696@users.noreply.github.com>
2026-02-21 12:06:31 +0530

    ready for backend deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,4 +10,8 @@ app.get("/health",(req,res) => {
   res.status(200).json({msg:"success api is up and running "})
 });
 
-app.listen(ENV.PORT,() => console.log("server is riunning on port:",ENV.PORT));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>
+  console.log("Server is running on port:", PORT)
+);
